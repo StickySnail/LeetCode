03e4e84edb731be6cc8c3371bfbcd985c2152468
Dong In Seo <dongin.seo.kr@gmail.com>
2024-03-23 15:56:13 -0500

    Time: 111 ms (58.11%), Space: 19 MB (73.41%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,20 @@
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         store = list()
+
+#         for num in nums:
+#             if num not in store:
+#                 store.append(num)
+#             else:
+#                 store.remove(num)
+
+#         return store[0]
+
+    
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        store = list()
-
+        xor = 0
         for num in nums:
-            if num not in store:
-                store.append(num)
-            else:
-                store.remove(num)
-
-        return store[0]
+            xor ^= num
+        
+        return xor
